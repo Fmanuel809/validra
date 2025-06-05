75a83119435f07daa82974de37e85b3e5ea74b16
Felix M. Martinez <felixmanuel396@gmail.com>
2025-06-05 19:32:18 -0400

    feat: agregar métodos estáticos de verificación de tipos en la clase TypeChecker

--- src/dls/helpers/type-checker.ts
+++ src/dls/helpers/type-checker.ts
@@ -1,5 +1,27 @@
-import { flatValues } from "@/utils";
 
 export class TypeChecker {
     
+    static isString(value: unknown): boolean {
+        return typeof value === 'string';
+    }
+
+    static isDate(value: unknown): boolean {
+        return value instanceof Date;
+    }
+
+    static isNumber(value: unknown): boolean {
+        return typeof value === 'number' && !isNaN(value);
+    }
+
+    static isBoolean(value: unknown): boolean {
+        return typeof value === 'boolean';
+    }
+
+    isArray(value: unknown): boolean {
+        return Array.isArray(value);
+    }
+
+    static isObject(value: unknown): boolean {
+        return value !== null && typeof value === 'object' && !Array.isArray(value);
+    }    
 }
